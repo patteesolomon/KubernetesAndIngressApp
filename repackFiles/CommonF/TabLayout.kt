3d1ccfc2badbaaabfec32d0179be534ffcd0d491
patteesolomon <theshiningkillerrashakublade@outlook.com>
2025-07-06 04:28:26 -0500

    LayoutTabMenu and more to come

--- repackFiles/CommonF/TabLayout.kt
+++ repackFiles/CommonF/TabLayout.kt
@@ -1,25 +1,59 @@
 package org.example.project
 
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.Top
+import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.Measurable
 import androidx.compose.ui.layout.MeasurePolicy
 import androidx.compose.ui.layout.MeasureResult
 import androidx.compose.ui.layout.MeasureScope
+import androidx.compose.ui.layout.Placeable.PlacementScope
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.LayoutDirection
 
 public class TabMeasurements : MeasurePolicy{
+    @Composable
+    fun MeasureScope.measure(
+        measurables: List<Measurable>,
+        constraints: Constraints,
+        alignmentLines: Map<AlignmentLine, Int>,
+        placementB: PlacementScope
+    ): MeasureResult {
+        return layout(
+            12, 4, alignmentLines,
+            placementBlock = {
+                var al = Top.align(10, 2);
+                var layoutD = LayoutDirection.Ltr;
+                var xe = placementB.coordinates;
+                var x = measurables[0];
+                var y = measurables[1];
+                var cons = constraints.maxWidth
+                // placementblock values go here
+                return@layout
+            }
+        )
+    }
+
     override fun MeasureScope.measure(
         measurables: List<Measurable>,
         constraints: Constraints
     ): MeasureResult {
-        TODO("Not yet implemented")
+        // todo
+        TODO();
     }
 
 }
 
-public class TabLayout {
-    var alignTab = Alignment.Top.align(10,4);
+@Composable
+fun TabLayout(){
+    var alignTab = Top.align(10,4);
     var layoutD = LayoutDirection.Ltr;
-    var textContent = "Commands";
+    Column {
+        Text(text = "Command")
+        Text(text = "Connection")
+        Text(text = "DataBase")
+        Text(text = "Config")
+    }
 }
